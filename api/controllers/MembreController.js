3b26c2b62e45e0f11f136eeca6b06d4138e0246f
spartan88 <davidcaillaud88@gmail.com>
2015-02-06 14:53:31 +0100

    Insertion Membre en BDD OK Travail sur l'affichage des message d'erreur

--- api/controllers/MembreController.js
+++ api/controllers/MembreController.js
@@ -7,10 +7,48 @@
 
 module.exports = {
 	
-        //Ajouter un utilisateur
-	ajouter: function(req, res) {	
+        //Affiche le formiulaire d'ajout utilisateur
+	ajouter: function(req, res) {
+                               
+            //On fait une copie de cette erreur
+            req.locals.flash = _.clone(req.session.flash);
+    
+            //En appelant la vue "ajouter.ejs" dans le dossier "view/Membre"
+            //et que l'action du controlleur s'appelle "ajouter"
+            //Pas besoin de fournir de parametres à res.view()
 	    res.view();
+            //On remet le message d'erreur à vide
+            req.session.flash = {};
 	},
+        
+        //Ajout d'un utilisateur
+        create : function (req, res, next){
+        
+            req.connection.remoteAddress;
+           
+            // On créé un Membre avec les paramétres du formulaire
+            Membre.create( req.params.all(), function membreCreated(err, membre){
+              
+                // Si une erreur intervient
+                // Erreur de validation suivant les critéres définit dans notre modéle
+                if(err) {
+                    //On affiche l'erreur sur notre consol NodeJs
+                   console.log(err);
+                   
+                   //On récupére l'erreur pour la retourner dans notre vue
+                   req.session.flash = {
+                      erreur: err
+                   }
+                
+                   //On redirige vers notre formulaire
+                   res.redirect("/membre/ajouter");                   
+                }
+                
+                res.json(membre);
+                //On remet le message d'erreur à vide si tous c'est bien passé
+                req.session.flash = {};
+            });     
+        },
         
         //Liste de tous les utilisateurs
 	lister: function(req, res) {	
--- api/models/Membre.js
+++ api/models/Membre.js
@@ -13,77 +13,96 @@ module.exports = {
     mem_id: {
         type: 'integer',
         primaryKey: true,
-        required: true,
-        unique: true,
+        unique: true
     },
     mem_pseudo: {
         type: 'string',
         required: true,
         unique: true,
+        minLength: 4, 
+        maxLength: 40,
+        size: 40
     },
     mem_mdp: {
         type: 'string',
         required: true,
+        minLength: 4, 
+        maxLength: 40,
+        size: 40
     },
     mem_mail: {
         type: 'email',
         required: true,
         unique: true,
-        email: true
+        email: true,
+        minLength: 4, 
+        maxLength: 60,
+        size: 60
     },
     mem_nom: {
         type: 'string',
         required: true,
+        minLength: 4, 
+        maxLength: 40,
+        size: 40
     },
     mem_prenom: {
         type: 'string',
         required: true,
+        minLength: 4, 
+        maxLength: 40,
+        size: 40
     },
     mem_date_naissance: {
-        type: 'datetime',
+        type: 'string',
         required: true,
+        size: 10
     },
     mem_adresse1: {
         type: 'text',
-        required: true,
+        required: true
     },
     mem_adresse2: {
-        type: 'text',
-        required: true,
+        type: 'text'
     },
     mem_cp: {
         type: 'string',
         required: true,
+        size: 5
     },
     mem_ville: {
         type: 'string',
         required: true,
+        size: 50
     },
     mem_ip: {
         type: 'string',
-        required: true,
         unique: true,
-        length: 12
+        size: 15,
+        defaultsTo: "127.0.0.1"
     },
     mem_facebook: {
         type: 'string',
-        required: true,
+        size: 255
     },
     mem_twitter: {
         type: 'string',
-        required: true,
+        size: 255
     },
     mem_xp: {
         type: 'integer',
-        required: true,
+        required: false,
+        defaultsTo: 0
     },
     mem_point: {
         type: 'integer',
-        required: true,
+        required: false,
+        defaultsTo: 0
     },
     mem_monaie: {
         type: 'integer',
-        required: true,
+        required: false,
+        defaultsTo: 0
     }
     
   }
