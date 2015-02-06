a27068a36bc77bc26b1e577d1b470a4a665edf95
spartan88 <davidcaillaud88@gmail.com>
2015-02-06 10:24:33 +0100

    Mise en place des vues de l'application

--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,7 @@ module.exports.routes = {
 
   '/': {
     view: 'User/index'
-  }
+  },
 
   /***************************************************************************
   *                                                                          *
@@ -45,5 +45,13 @@ module.exports.routes = {
   * for configuration options and examples.                                  *
   *                                                                          *
   ***************************************************************************/
+  '/lister': {
+    view: 'User/liste'
+  },
+  
+    '/ajouter': {
+    view: 'User/ajouter'
+  }
+
 
 };
